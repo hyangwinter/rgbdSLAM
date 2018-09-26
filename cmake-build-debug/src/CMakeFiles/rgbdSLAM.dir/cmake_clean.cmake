a4e0a816fd0333aff703b96c2dc057d96e91c18c
hyangwinter <824141161@qq.com>
2018-09-26 11:26:12 +0800

    slam base

---
+++ cmake-build-debug/src/CMakeFiles/rgbdSLAM.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rgbdSLAM.dir/main.cpp.o"
+  "rgbdSLAM.pdb"
+  "rgbdSLAM"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rgbdSLAM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
